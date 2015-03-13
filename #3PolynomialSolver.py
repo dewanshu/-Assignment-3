773aeca59f0d4e24c03ac79924804cdaf205dfb8
muraritoshniwal <muraritoshniwal@users.noreply.github.com>
2015-03-13 20:58:18 +0530

    #3(i)done.. nearly completed

--- #3PolynomialSolver.py
+++ #3PolynomialSolver.py
@@ -45,3 +45,23 @@ if(method=='newtonraphson'):
 			return(l)
 		else:
 			return NULL
+
+
+
+def solve(self,n,L,method):
+		if(method=='bisectionsearch'):
+			print("Enter lower bound of interval in which root lies")
+			l=int(input())
+			print("Enter upper bound of interval in which root lies")
+			u=int(input())
+			print("Enter maximum number of itertions")
+			it=int(input())
+			while(abs(self.F(n,L,l)-self.F(n,L,u))>0.00001 and it>0):
+				m=(l+u)/2
+				if(self.F(n,L,l)*self.F(n,L,m)<0):
+					u=m
+				else:
+					l=m
+				print (l,u,self.F(n,L,l),self.F(n,L,u))
+				it-=1
+			return([l,u])
