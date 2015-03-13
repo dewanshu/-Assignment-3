efb5e776d6ba832ea83f50a9a99c3f4eb8d48027
ayush1996 <kedia.ayush24@gmail.com>
2015-03-13 21:48:11 +0530

    Completed!!!

--- LPsolver.py
+++ LPsolver.py
@@ -1,41 +1,62 @@
-class LPsolver:
-    def solve(self, a, b, c):
-        C, B, x, maxPos, minPos = [], [], [], [], []
-        for i in c:
-            C += [[j for j in i]]
-        x= [0 for j in range(len(a))]    
-        for i in range(len(b)):
-            C[i] += [0 for i in range(len(b))]
-            C[i][len(a)+i] = 1
-        a += [0 for i in range(len(b))]
-        B = [0 for i in range(len(b))]
-        while True:
-            maxPos, minPos = [], []
-            for i in range(len(a)):
-                sum = 0
-                for j in range(len(b)):
-                    sum += C[j][i]*B[j]
-                maxPos += [a[i]-sum]
-            counter = 0
-            for i in maxPos:
-                if i <= 0:
-                     counter += 1
-            if counter == len(a):
-                 break
-            index1 = maxPos.index(max(maxPos))    
-            for i in range(len(b)):
-                if C[i][index1] != 0:
-                     minPos += [b[i]/C[i][index1]]
-                elif b[i] > 0:
-                    minPos += ["infinity"]
+import numpy as np
+
+objective=[1,3]
+constraint1=[10,5,4]
+constraint2=[(1,2),(1,0),(0,1)]
+
+class LPsolver():
+
+    def solve(self,a,b,c):
+        lb=len(b)
+        la=len(a)
+        arr=np.array(c)
+        arr=np.concatenate((arr,np.identity(len(b))),1)
+        b=np.array(b)
+        x=[0]*l_a
+
+        arr=np.insert(arr,len(a)+lb,b,1)
+        B=np.array([0]*lb)
+        C=np.array(a+[0]*lb)
+        d=range(la,la+lb)
+        while(1):
+            maxpos=[]
+            minpos=[]
+            for i in range(la+lb):
+                maxpos.append(C[i]-np.dot(B,arr[:,i]))
+
+
+            c=maxpos.index(max(maxpos))
+
+            if maxpos[c]<=0:
+                break
+            for i in range(lb):
+                if arr[i,c]!=0:
+                    minpos.append(arr[i,-1]/arr[i,c])
                 else:
-                    minPos += ["-infinity"]
-            counter = 0
-            for i in minPos:
-                if i != "infinity":
-                    if i == "-infinity":
-                        counter += 1
-                    elif i <= 0:
-                        counter += 1
-            if counter == len(b):
-                return "problem is unbounded"
+                    minpos.append(10000000000)
+            q=filter(lambda x:x>0,minpos)
+            if q==[]:
+                return "unbounded function"
+                break
+            r=minpos.index(min(q))
+
+            B[r]=C[c]
+            d[r]=c
+            arr[r,:]=arr[r,:]/arr[r,c]
+            for i in range(lb):
+                if(i!=r):
+                    arr[i,:]=arr[i,:]-(arr[i,c]/arr[r,c])*arr[r,:]
+
+        for i in range(len(d)):
+            if d[i]<l_a:
+                x[d[i]]=arr[i,-1]
+        x=map(int,x)
+
+        x.append(sum([x[i]*a[i] for i in range(la)]))
+        return x
+
+
+lps=LPsolver()
+solution=lps.solve(objective,constraint1,constraint2)
+print solution
+
